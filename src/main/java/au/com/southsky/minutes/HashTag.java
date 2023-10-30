5e99bb71db1ad636533b41180d0f2d95220c90e1
Steven Ring <smr@southsky.com.au>
2023-10-30 14:12:17 +1100

    Introduce Regex

--- src/main/java/au/com/southsky/minutes/HashTag.java
+++ src/main/java/au/com/southsky/minutes/HashTag.java
@@ -1,24 +1,26 @@
 package au.com.southsky.minutes;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class HashTag {
 
     public static ArrayList <HashTag> tagList;
     private String tagName;
-    private String pattern;
+    private Pattern pattern;
 
-    public HashTag(String tagName, String pattern) {
+    public HashTag(String tagName, String regex) {
         this.tagName = tagName;
-        this.pattern = pattern;
+        this.pattern = Pattern.compile(regex);
     }
 
     public String toString() {
-        return "Hashtag: #"+this.tagName+" Pattern: "+this.tagName;
+        return "Hashtag: #"+this.tagName;
     }
 
     public static void loadTagList(java.io.InputStream input) throws InvalidHashTagException {
@@ -32,14 +34,28 @@ public class HashTag {
                 System.out.println(line);
                 String [] fields = line.split(" ");
                 System.out.println(fields.length);
-                if (fields.length != 2) {
-                    throw new InvalidHashTagException("Expected two fields in: " + line);
+                if (fields.length != 1) {
+                    throw new InvalidHashTagException("Expected one fields in: " + line);
                 }
+                String tagName = extractTagName(line);
+                String pattern = line.split("=")[1];
+                tagList.add(new HashTag(tagName, pattern));
                 line = reader.readLine();
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String extractTagName(String pattern) throws InvalidHashTagException {
+
+        if (!pattern.startsWith("#")) {
+            throw new InvalidHashTagException("pattern must start with '#' at " + pattern);
+        }
+        if (!pattern.contains("=")) {
+            throw new InvalidHashTagException("pattern must contain an '=' at " + pattern);
+        }
+        return pattern.split("=")[0].substring(0);
     }
 }
